1a6862dc39a6fec96cf8d30ba95470c12a4348a7
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-02-13 10:51:18 +0100

    Adds elevator driver

--- src/main.c
+++ src/main.c
@@ -1,7 +1,32 @@
+#include "elev.h"
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello world\n");
-    printf("Hei Hei\n");
-    printf("\n");
+
+int main() {
+    // Initialize hardware
+    if (!elev_init()) {
+        printf("Unable to initialize elevator hardware!\n");
+        return 1;
+    }
+
+    printf("Press STOP button to stop elevator and exit program.\n");
+
+    elev_set_motor_direction(DIRN_UP);
+
+    while (1) {
+        // Change direction when we reach top/bottom floor
+        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
+            elev_set_motor_direction(DIRN_DOWN);
+        } else if (elev_get_floor_sensor_signal() == 0) {
+            elev_set_motor_direction(DIRN_UP);
+        }
+
+        // Stop elevator and exit program if the stop button is pressed
+        if (elev_get_stop_signal()) {
+            elev_set_motor_direction(DIRN_STOP);
+            break;
+        }
+    }
+
+    return 0;
 }
