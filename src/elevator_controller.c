527869ecc876a914df2b096cbeb0ebf6cb588ecc
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-02-18 18:07:45 +0100

    Adds state_data

--- src/elevator_controller.c
+++ src/elevator_controller.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include "fsm.h"
 
-
+///Current running state
 fsm_state_e current_state;
+///Next desired state
 fsm_state_e next_state;
 
 ///Contains all state functions and transition functions
@@ -16,17 +17,18 @@ fsm_state_func state_table[FSM_NUM_STATES][FSM_NUM_STATES] =
     {}
 };
 
-void elevator_controller_loop_once() {
-    state_data_s data;
+void elevator_controller_loop_once(state_data_s* state_data) {
+    ///Get next state function
     fsm_state_func func = state_table[current_state][next_state];
     current_state = next_state;
     if(func != NULL) {
-        next_state = func(data);
+        ///Run current state
+        next_state = func(state_data);
     }
 }
 
 void emergency_stop() {
-
+    next_state = STATE_EMERGENCY;
 }
 
 
