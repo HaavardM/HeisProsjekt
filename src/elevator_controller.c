16b83f90fa9983e7068acf8a75badec7d9f04bbf
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-02-18 17:10:06 +0100

    Fix build errors

--- src/elevator_controller.c
+++ src/elevator_controller.c
@@ -1,4 +1,5 @@
 #include "elevator_controller.h"
+#include <stdio.h>
 #include "fsm.h"
 
 
@@ -13,14 +14,14 @@ fsm_state_func state_table[FSM_NUM_STATES][FSM_NUM_STATES] =
     {},
     {},
     {}
-}
+};
 
 void elevator_controller_loop_once() {
-    
+    state_data_s data;
     fsm_state_func func = state_table[current_state][next_state];
     current_state = next_state;
     if(func != NULL) {
-        next_state = func();
+        next_state = func(data);
     }
 }
 
