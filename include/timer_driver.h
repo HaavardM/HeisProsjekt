a174813e8a3749952f581838d73c0c977ab93806
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-02-20 19:49:49 +0100

    Follow naming convention

--- include/timer_driver.h
+++ include/timer_driver.h
@@ -7,19 +7,19 @@
 typedef struct { 
     clock_t start_time;
     clock_t duration_ms;
-} timer_s;
+} timer_t;
 
 
 /**Create timer
  * @param d_ms Timer duration in milliseconds
  * @return Timer struct
  */
-timer_s create_timer(unsigned int d_ms);
+timer_t create_timer(unsigned int d_ms);
 /**Check if timer has elapsed
  * @param timer Timer to check
  * @return Has timer elapsed or not
  */
-bool timer_has_elapsed(timer_s timer);
+bool timer_has_elapsed(timer_t timer);
 
 
 #endif
--- src/timer_driver.c
+++ src/timer_driver.c
@@ -1,13 +1,13 @@
 #include "timer_driver.h"
 
-timer_s create_timer(unsigned int d_ms) {
-    timer_s temp;
+timer_t create_timer(unsigned int d_ms) {
+    timer_t temp;
     temp.duration_ms = d_ms;
     temp.start_time = clock(); 
     return temp;
 }
 
-bool timer_has_elapsed(timer_s timer) {
+bool timer_has_elapsed(timer_t timer) {
     clock_t now = clock();
     long elapsed = (((double)(now - timer.start_time)) / CLOCKS_PER_SEC) * 1000;
     return elapsed > timer.duration_ms;
