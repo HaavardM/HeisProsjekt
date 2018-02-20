5eb04e71fa6773c55bf4613d676fce1b4f3d2543
Emma Horn Buøen <emmahb@stud.ntnu.no>
2018-02-20 11:23:02 +0100

    Updated door_driver module

--- include/elevator_controller.h
+++ include/elevator_controller.h
@@ -1,11 +1,13 @@
 #ifndef ELEVATOR_CONTROLLER_H
 #define ELEVATOR_CONTROLLER_H
 
+#include "fsm.h"
+
 ///Run emergency stop
 void emergency_stop();
 
 ///Run current elevator state once
-void elevator_controller_loop_once();
+void elevator_controller_loop_once(const state_data_s* state_data);
 
 
 #endif
