8e68c8f1ad82838b8bfe5ff53d0bb7c89473ecd4
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-02-26 22:19:09 +0100

    Improves order queue code

--- include/order_queue.h
+++ include/order_queue.h
@@ -6,12 +6,34 @@
 #define HEISPROSJEKT_ORDER_QUEUE_H
 #include "motor_defines.h"
 
-
+/** Add up order from floor
+ *  @param floor - Which floor ordered from
+ */
 void add_to_order_queue_up(int floor);
+
+/** Add down order from floor
+ * @param floor - Which floor ordered from
+ */
 void add_to_order_queue_down(int floor);
+
+///Empty all queues
 void empty_queue(void);
-int get_next_order(int cuurent_floor, motor_direction_e dir);
+
+/** Get next order from queues
+ * @param current_floor
+ * @param dir motor direction
+ * @return next floor to stop, -1 if no orders in queue
+ */
+int get_next_order(int current_floor, motor_direction_e dir);
+
+/** Add down order from floor
+ * @param floor - Which floor ordered from
+ */
 void add_to_order_queue_dest(int floor);
+
+/** Clear all orders from given floor
+ * @param floor Floor to clear
+ */
 void clear_order_in_queue(int floor);
 
 
