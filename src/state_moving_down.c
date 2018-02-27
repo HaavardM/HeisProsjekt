74f79628b99483543897a6d8407f3e1b46c973f7
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-02-27 12:46:25 +0100

    Fix edgecase

--- src/state_moving_down.c
+++ src/state_moving_down.c
@@ -17,10 +17,16 @@ fsm_state_e state_moving_down_entry(const state_data_t* state_data_p)  {
 }
 fsm_state_e state_moving_down_do(const state_data_t* state_data_p) {
     if (state_data_p ->current_floor == state_data_p ->target_floor && state_data_p->target_floor != -1) {
+        //If at target floor, stop
         return STATE_AT_FLOOR;
     } else if(state_data_p->last_floor == -1 && state_data_p->current_floor != -1) { 
+        //If no target floor, stop
         return STATE_AT_FLOOR;
+    } else if(state_data_p->current_floor < state_data_p->target_floor && state_data_p->current_floor != -1) {
+        //If at end floor, go back up (edge case)
+        return STATE_MOVING_UP;
     } else {
+        //Else continue
         return STATE_MOVING_DOWN;
     }
 
