ce267a72b13987e4bf5075a74f543438e2062926
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-02-27 10:27:15 +0100

    Close door in execute queue

--- src/state_moving_down.c
+++ src/state_moving_down.c
@@ -10,7 +10,6 @@
 
 fsm_state_e state_moving_down_entry(const state_data_t* state_data_p)  {
     printf("State moving down entry\n");
-    close_door();
     set_motor_direction(MOTOR_DIRECTION_DOWN);
     start_motor();
     return STATE_MOVING_DOWN;
--- src/state_moving_up.c
+++ src/state_moving_up.c
@@ -10,7 +10,6 @@
 
 fsm_state_e state_moving_up_entry(const state_data_t* state_data_p)  {
     printf("State moving up entry\n");
-    close_door();
     set_motor_direction(MOTOR_DIRECTION_UP);
     start_motor();
     return STATE_MOVING_UP;
