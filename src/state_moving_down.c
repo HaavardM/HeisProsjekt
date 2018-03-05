b4c19b0c0efdf71dd0ddfbbb35c920f28e67513e
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-03-05 14:23:39 +0100

    Avoid use of direct constants

--- src/state_moving_down.c
+++ src/state_moving_down.c
@@ -8,6 +8,8 @@
 #include "fsm.h"
 #include <stdio.h>
 
+#define INVALID_FLOOR -1
+
 fsm_state_e state_moving_down_entry(const state_data_t* state_data_p)  {
     printf("State moving down entry\n");
     set_motor_direction(MOTOR_DIRECTION_DOWN);
@@ -16,13 +18,13 @@ fsm_state_e state_moving_down_entry(const state_data_t* state_data_p)  {
 
 }
 fsm_state_e state_moving_down_do(const state_data_t* state_data_p) {
-    if (state_data_p ->current_floor == state_data_p ->target_floor && state_data_p->target_floor != -1) {
+    if (state_data_p ->current_floor == state_data_p ->target_floor && state_data_p->target_floor != INVALID_FLOOR) {
         //If at target floor, stop
         return STATE_AT_FLOOR;
-    } else if(state_data_p->last_floor == -1 && state_data_p->current_floor != -1) { 
+    } else if(state_data_p->last_floor == INVALID_FLOOR && state_data_p->current_floor != INVALID_FLOOR) { 
         //If no target floor, stop
         return STATE_AT_FLOOR;
-    } else if(state_data_p->current_floor < state_data_p->target_floor && state_data_p->current_floor != -1) {
+    } else if(state_data_p->current_floor < state_data_p->target_floor && state_data_p->current_floor != INVALID_FLOOR) {
         //If going the wrong way, switch direction
         return STATE_MOVING_UP;
     } else {
