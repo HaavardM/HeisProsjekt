32edeb3587fbe67f31410f173448801b62a95de0
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-03-06 10:28:05 +0100

    Improve state emergency

--- src/state_emergency.c
+++ src/state_emergency.c
@@ -9,19 +9,26 @@
 
 fsm_state_e state_emergency_entry(const state_data_t* state_data_p) {
     printf("State emergency entry\n");
-    if(state_data_p->current_floor != -1) {
-        open_door();
-    }
     stop_motor();
     return STATE_EMERGENCY;
 }
 
 fsm_state_e state_emergency_do(const state_data_t* state_data_p) {
+    //Clear queue
     empty_queue();
+    //Clear lights
     for(int i = 0; i < NUM_FLOORS; ++i) {
         clear_elevator_light(i);
         clear_floor_light(i);
     }
+    //If we're at a floor, open the doors
+    if(state_data_p->current_floor != -1) {
+        open_door();
+    } else {
+        //If not, close
+        close_door();
+    }
+    
     if (state_data_p->emergency_button_pressed) {
         return STATE_EMERGENCY;
     } else if (state_data_p->current_floor == -1) {
